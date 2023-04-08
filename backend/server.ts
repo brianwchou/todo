50f5176aa3796e15b4dd543564600136f9c94e60
Brian Chou <bchou515@gmail.com>
2023-04-08 18:11:37 -0400

    can write to database

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express, { json, urlencoded } from "express";
 import { PrismaClient } from "@prisma/client";
-// import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 const saltRounds = 10;
 
 const prisma = new PrismaClient();
@@ -23,23 +23,52 @@ app.get("/", async (req, res) => {
 
 app.post("/createuser", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-
-  const passwordRe = new RegExp(
+  const passwordReg = new RegExp(
     "^(?=.*d)(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[-#$.%&*])(?=.*[a-zA-Z]).{8,16}$"
   );
-  if (passwordRe.test(password)) {
-    res.status(400).json({ passwordError: "password is missing letters" });
+  const emailReg = new RegExp("^([w.-]+)@([w-]+)((.(w){2,3})+)$");
+
+  type Error = { type: string; message: string };
+  let errors: Error[] = [];
+
+  if (!password.length) {
+    errors.push({
+      type: "password",
+      message: "passwordfield is empty",
+    });
+  } else if (passwordReg.test(password)) {
+    errors.push({
+      type: "password",
+      message: "something wrong with your password",
+    });
   }
 
-  // validate password
-  // validate email
+  if (!email.length) {
+    errors.push({ type: "email", message: "emailfield is empty" });
+  } else if (emailReg.test(email)) {
+    errors.push({ type: "email", message: "something wrong with your email" });
+  }
 
-  // bcrypt.genSalt(saltRounds, function (error, salt) {
-  //   bcrypt.hash("blahblah", salt, function (err, hash) {
-  //     prisma
-  //     console.log(`hash:${hash} salt:${salt}`);
-  //   });
-  // });
+  if (errors.length > 0) {
+    res.status(400).json(errors);
+    return;
+  }
+
+  console.log("RUNNING BCRYPT");
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+
+  const user = await prisma.user.create({
+    data: {
+      name: firstName + " " + lastName,
+      email,
+      passwordHash: hash,
+      salt,
+    },
+  });
+  console.log(user);
+
+  res.status(200).send("we good");
 });
 
 app.listen(PORT, () => {
